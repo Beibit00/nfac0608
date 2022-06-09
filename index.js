a0a167c94ab42e664ecc9c99e6f46ff743fd7fb1
Beibit00 <beibit001015@gmail.com>
2022-06-09 19:38:40 +0600

    silver

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ let playVideoBtn = document.getElementById("btn-change-background")
 const playPauseTrackBtn = document.getElementById("playpause-track")
 const audio = document.getElementById("audio")
 const progressBar = document.getElementById("progressBar")
+
+
 // background.removeAttribute('controls');
 // controls.style.visibility = 'visible';
 // playVideoBtn.addEventListener("click", (e)=>{
@@ -23,57 +25,78 @@ function playVideo() {
   }
 }
 
+const audioTest = new Audio("./assets/kui---erke-sylkym_(muzsoul.net).mp3");
 
-function playPauseTrack() {
-  if (audio.paused) {
-    audio.play();
-    playPauseTrackBtn.innerHTML = '<i class="fa fa-pause-circle fa-3x"></i>';
-  } else {
-    audio.pause();
+const playPauseTrack = () => {
+ 
+  if (!audioTest.paused) {
+    audioTest.pause();
     playPauseTrackBtn.innerHTML = '<i class="fa fa-play-circle fa-3x"></i>';
-
+  } else {
+    audioTest.play();
+    playPauseTrackBtn.innerHTML = '<i class="fa fa-pause-circle fa-3x"></i>';
   }
+  // if (audio.paused) {
+  //   audio.play();
+  //   playPauseTrackBtn.innerHTML = '<i class="fa fa-pause-circle fa-3x"></i>';
+  // } else {
+  //   audio.pause();
+  //   playPauseTrackBtn.innerHTML = '<i class="fa fa-play-circle fa-3x"></i>';
+
+  // }
 }
 
 
-// function progressValue() {
+function progressValue() {
 
-//   progressBar.max = track.duration;
-//   progressBar.value = track.currentTime;
+  progressBar.max = audioTest.duration;
+  progressBar.value = audioTest.currentTime;
 
-//   currentTime.textContent = formatTime(track.currentTime);
-//   durationTime.textContent = formatTime(track.duration);
-// }
-
-// setInterval(progressValue, 500);
-
-// function formatTime(sec) {
-//   let minutes = Math.floor(sec / 60);
-//   let seconds = Math.floor(sec - minutes * 60);
-//   if (seconds < 10) {
-//     seconds = `0${seconds}`;
-//   }
-//   return `${minutes}:${seconds}`;
-// }
-
-// function changeProgressBar() {
-//   track.currentTime = progressBar.value;
-// }
-
-// progressBar.addEventListener("click", changeProgressBar);
+  document.getElementById('currentTime').innerHTML = (formatTime(Math.floor(audioTest.currentTime)));
+  if (document.getElementById('durationTime').innerHTML === "NaN:NaN") {
+    document.getElementById('durationTime').innerHTML = "0:00";
+  } else {
+    document.getElementById('durationTime').innerHTML = (formatTime(Math.floor(audioTest.duration)));
+  }
 
 
 
+  // currentTime.textContent = formatTime(track.currentTime);
+  // durationTime.textContent = formatTime(track.duration);
+}
+function formatTime(seconds) {
+  let min = Math.floor((seconds / 60));
+  let sec = Math.floor(seconds - (min * 60));
+  if (sec < 10) {
+    sec = `0${sec}`;
+  };
+  return `${min}:${sec}`;
+};
+
+setInterval(progressValue, 500);
 
 
-document.addEventListener('keyup', (event) => {
-  var name = event.key;
-  var code = event.code;
-  // Alert the key name and key code on keydown
-  alert(`Key pressed ${name} \r\n Key code value: ${code}`);
-}, false);
+
+function changeProgressBar() {
+  audioTest.currentTime = progressBar.value;
+}
+
+
+
+
+
+
+
+// document.addEventListener('keyup', (event) => {
+//   var name = event.key;
+//   var code = event.code;
+//   // Alert the key name and key code on keydown
+//   alert(`Key pressed ${name} \r\n Key code value: ${code}`);
+// }, false);
 
 
 playPauseTrackBtn.addEventListener("click", playPauseTrack);
+
+progressBar.addEventListener("click", changeProgressBar);
 
 background.addEventListener("click", playVideo);
